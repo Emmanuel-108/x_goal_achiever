6a13b4d00cdad4903727ee30c1806a2fea142216
Emanuel Flores <flores.emanuel.108@gmail.com>
2025-05-13 21:24:50 -0400

    contact and tutorials routes and controllers added + vieews

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :tutorial ]
 
   def home
+  end
+
+  def tutorial
   end
 
   def contact
