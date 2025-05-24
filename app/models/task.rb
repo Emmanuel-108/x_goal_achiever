7bdc150d6a56e799b5ee2968a8ef9be81805eda9
Emanuel Flores <flores.emanuel.108@gmail.com>
2025-05-24 11:41:34 -0400

    subtask randomness added

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,32 @@ class Task < ApplicationRecord
   has_many :subtasks, dependent: :destroy
   accepts_nested_attributes_for :subtasks, allow_destroy: true
   has_many :statistics, as: :input
+
+  def distribute_time!(total_time, distribution_type)
+    count = subtasks.size
+    return if count.zero?
+
+    times =
+      case distribution_type
+      when "even"
+        Array.new(count, total_time / count)
+      when "incremental"
+        base = total_time / (count * (count + 1) / 2.0)
+        (1..count).map { |i| (base * i).round }
+      when "decremental"
+        base = total_time / (count * (count + 1) / 2.0)
+        (1..count).to_a.reverse.map { |i| (base * i).round }
+      when "random"
+        # Random distribution that sums to total_time
+        randoms = Array.new(count) { rand }
+        sum = randoms.sum
+        randoms.map { |r| (r / sum * total_time).round }
+      else
+        []
+      end
+
+    subtasks.each_with_index do |subtask, index|
+      subtask.time = times[index].minutes.from_now
+    end
+  end
 end
